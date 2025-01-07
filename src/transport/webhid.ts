fb33fbce63f36e3703a128c546767766a96f6ac6
Geno <geno@live.com>
2025-01-07 20:42:52 +0800

    Update webhid.ts

--- src/transport/webhid.ts
+++ src/transport/webhid.ts
@@ -9,7 +9,6 @@ const REPORT_ID = 0x90;
  * WebHID Transport class
  */
 export class WebHID implements Transport {
-    private device?: HIDDevice;
     private reportId?: number;
     public readonly packetSize = 64;
 
