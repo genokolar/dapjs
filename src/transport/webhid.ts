12710453e99ed241524ea4fc482df22efc5082a3
Geno <geno@live.com>
2025-01-07 20:03:30 +0800

    添加WEBHID

---
+++ src/transport/webhid.ts
@@ -0,0 +1,63 @@
+import { Transport } from './';
+
+interface HIDDevice {
+    open(): Promise<void>;
+    close(): Promise<void>;
+    receiveReport(reportId: number): Promise<ArrayBuffer>;
+    sendReport(reportId: number, data: Uint8Array): Promise<void>;
+}
+
+/**
+ * @hidden
+ */
+const REPORT_ID = 0x90;
+
+/**
+ * WebHID Transport class
+ */
+export class WebHID implements Transport {
+    public readonly packetSize = 64;
+
+    /**
+     * WebHID constructor
+     * @param device WebHID device to use
+     * @param reportId Report ID to use
+     */
+    constructor(private device: HIDDevice, private reportId = REPORT_ID) {
+    }
+
+    /**
+     * Open device
+     * @returns Promise
+     */
+    public async open(): Promise<void> {
+        await this.device.open();
+    }
+
+    /**
+     * Close device
+     * @returns Promise
+     */
+    public async close(): Promise<void> {
+        await this.device.close();
+    }
+
+    /**
+     * Read from device
+     * @returns Promise of DataView
+     */
+    public async read(): Promise<DataView> {
+        const report = await this.device.receiveReport(this.reportId);
+        return new DataView(report);
+    }
+
+    /**
+     * Write to device
+     * @param data Data to write
+     * @returns Promise
+     */
+    public async write(data: BufferSource): Promise<void> {
+        const buffer = new Uint8Array(data as ArrayBuffer);
+        await this.device.sendReport(this.reportId, buffer);
+    }
+}
